6433e56335096c55af9debd78665102093b1b1d4
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-27 16:26:58 +0100

    Fine esercizio

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppTitle from './components/AppTitle.vue'
+
+export default {
+  components: {
+    AppTitle
+    
+  }
+}
+</script>
+
+<template>
+  <div>
+   <h2>Ciao ecco vite</h2>
+  </div>
+  <AppTitle/>
+</template>
+
+<style scoped>
+
+</style>
